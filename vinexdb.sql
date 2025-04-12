ce2dcf02533298c1ad0dc8a0649493f33123d25b
bag.xml <82437913+bag-xml@users.noreply.github.com>
2025-04-12 12:54:51 +0200

    update db after a while

--- vinexdb.sql
+++ vinexdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Aug 24, 2024 at 03:07 PM
+-- Generation Time: Apr 12, 2025 at 12:53 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,6 +24,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `comments`
+--
+
+CREATE TABLE `comments` (
+  `commentID` int(11) NOT NULL,
+  `authorID` int(11) NOT NULL,
+  `postID` int(11) NOT NULL,
+  `comment` varchar(500) NOT NULL,
+  `creationDate` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `likes`
+--
+
+CREATE TABLE `likes` (
+  `likeId` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `postID` int(11) NOT NULL,
+  `username` varchar(32) NOT NULL,
+  `avatarURL` varchar(500) NOT NULL,
+  `creationDate` varchar(25) NOT NULL,
+  `location` varchar(80) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `notifications`
 --
 
@@ -37,9 +67,35 @@ CREATE TABLE `notifications` (
   `sender_username` varchar(50) NOT NULL,
   `message` varchar(100) NOT NULL,
   `creationDate` varchar(25) NOT NULL,
-  `avatarURL` varchar(500) NOT NULL,
+  `avatarURL` varchar(500) NOT NULL DEFAULT 'http://vine-x.bag-xml.com/static/pfps/7bc6c93de0ad70ec51a42f5c6277be2496d4c90b0a4ad00b726f096d0e661797.png',
   `thumbnailURL` varchar(500) NOT NULL,
   `adminMessage` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `postID` int(11) NOT NULL,
+  `authorID` int(11) NOT NULL,
+  `authorName` varchar(50) NOT NULL,
+  `thumbnailURL` varchar(500) NOT NULL,
+  `videoURL` varchar(500) NOT NULL,
+  `location` varchar(120) NOT NULL,
+  `description` varchar(80) NOT NULL,
+  `creationDate` varchar(25) NOT NULL,
+  `comments` longtext NOT NULL,
+  `likes` longtext NOT NULL,
+  `tags` longtext NOT NULL,
+  `usersWhoLiked` longtext NOT NULL DEFAULT '{"liked": []}',
+  `verified` tinyint(1) NOT NULL DEFAULT 0,
+  `promoted` tinyint(1) NOT NULL DEFAULT 0,
+  `postToFacebook` int(11) NOT NULL DEFAULT 0,
+  `foursquareVenueID` int(11) NOT NULL DEFAULT 0,
+  `authorPFP` longtext NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -56,7 +112,7 @@ CREATE TABLE `users` (
   `phoneNumber` int(11) NOT NULL,
   `username` varchar(32) NOT NULL,
   `description` varchar(60) NOT NULL,
-  `pfp` varchar(500) NOT NULL,
+  `pfp` longtext NOT NULL,
   `location` varchar(32) NOT NULL,
   `followingCount` int(11) NOT NULL,
   `followerCount` int(11) NOT NULL,
@@ -65,10 +121,13 @@ CREATE TABLE `users` (
   `blocked` longtext NOT NULL DEFAULT '{"blocked":[]}',
   `following` longtext NOT NULL DEFAULT '{"following":[]}',
   `followers` longtext NOT NULL DEFAULT '{"followers":[]}',
+  `likedPosts` longtext NOT NULL DEFAULT '{"liked_posts":[]}',
+  `pending_notifications_count` int(11) NOT NULL DEFAULT 0,
   `promo` tinyint(1) NOT NULL DEFAULT 0,
   `isPrivate` tinyint(1) NOT NULL DEFAULT 0,
   `isVerified` tinyint(1) NOT NULL DEFAULT 0,
   `isAdmin` tinyint(1) NOT NULL DEFAULT 0,
+  `AdminKey` varchar(12) NOT NULL,
   `isBanned` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
@@ -77,10 +136,28 @@ CREATE TABLE `users` (
 --
 
 --
+-- Indexes for table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`commentID`);
+
+--
+-- Indexes for table `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`likeId`);
+
+--
 -- Indexes for table `notifications`
 --
 ALTER TABLE `notifications`
   ADD PRIMARY KEY (`notificationID`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`postID`);
 
 --
 -- Indexes for table `users`
@@ -93,10 +170,28 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `commentID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `likeId` int(11) NOT NULL AUTO_INCREMENT;
+
+--
 -- AUTO_INCREMENT for table `notifications`
 --
 ALTER TABLE `notifications`
   MODIFY `notificationID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `postID` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `users`
